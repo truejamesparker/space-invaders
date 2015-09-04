d4112e82bc957d5847111385a685196c93005af2
asdfa <asdfakf>
2015-09-04 16:20:10 -0600

    Added lab02

---
+++ lab02/workspace/real_time_clock/Debug/src/main.d
@@ -0,0 +1,39 @@
+src/main.d src/main.o: ../src/main.c \
+  ../../hello_world_bsp_0/microblaze_0/include/xgpio.h \
+  ../../hello_world_bsp_0/microblaze_0/include/xil_types.h \
+  ../../hello_world_bsp_0/microblaze_0/include/xil_assert.h \
+  ../../hello_world_bsp_0/microblaze_0/include/xstatus.h \
+  ../../hello_world_bsp_0/microblaze_0/include/xbasic_types.h \
+  ../../hello_world_bsp_0/microblaze_0/include/xgpio_l.h \
+  ../../hello_world_bsp_0/microblaze_0/include/xil_io.h \
+  ../../hello_world_bsp_0/microblaze_0/include/mb_interface.h \
+  ../src/platform.h ../src/platform_config.h \
+  ../../hello_world_bsp_0/microblaze_0/include/mb_interface.h \
+  ../../hello_world_bsp_0/microblaze_0/include/xintc_l.h \
+  ../../hello_world_bsp_0/microblaze_0/include/xparameters.h
+
+../../hello_world_bsp_0/microblaze_0/include/xgpio.h:
+
+../../hello_world_bsp_0/microblaze_0/include/xil_types.h:
+
+../../hello_world_bsp_0/microblaze_0/include/xil_assert.h:
+
+../../hello_world_bsp_0/microblaze_0/include/xstatus.h:
+
+../../hello_world_bsp_0/microblaze_0/include/xbasic_types.h:
+
+../../hello_world_bsp_0/microblaze_0/include/xgpio_l.h:
+
+../../hello_world_bsp_0/microblaze_0/include/xil_io.h:
+
+../../hello_world_bsp_0/microblaze_0/include/mb_interface.h:
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../hello_world_bsp_0/microblaze_0/include/mb_interface.h:
+
+../../hello_world_bsp_0/microblaze_0/include/xintc_l.h:
+
+../../hello_world_bsp_0/microblaze_0/include/xparameters.h:
