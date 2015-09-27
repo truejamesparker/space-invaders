96a58d6816ed7c4be20ef62d69e4fc3268ec2997
asdfa <asdfakf>
2015-09-26 21:57:07 -0600

    added missile stuff

--- sw/space_invaders/src/elements/missiles.c
+++ sw/space_invaders/src/elements/missiles.c
@@ -6,3 +6,18 @@
  */
 
 #include "missiles.h"
+
+void missiles_move(){
+	int i;
+	for(i=0; i<MISSILE_COUNT; i++){
+		missile_t missile = missile_array[i];
+		if (missile.active){
+			if(missile.up){
+				screen_drawSymbol(missile.symbol, missile.point, missile.size,
+														MISSILE_SCALE, 0x0000);
+				screen_drawSymbol(missile.symbol, missile.point, missile.size,
+						MISSILE_SCALE, 0xFFFFFFFF);
+			}
+		}
+	}
+}
--- sw/space_invaders/src/elements/missiles.h
+++ sw/space_invaders/src/elements/missiles.h
@@ -13,10 +13,13 @@
 #define MISSILE_HEIGHT 		6
 #define MISSILE_WIDTH 		6
 #define MISSILE_COUNT		4
+#define MISSILE_SCALE		SCALE
+#define MISSILE_SHIFT 		SCALE * 2
 
 typedef struct{
-	const uint32_t* symbol;
+	uint32_t* symbol;
 	const bool up;
+	bool active;
 	const symbolsize_t size;
 } missile_t;
 
@@ -26,21 +29,29 @@ void missiles_tank_fire();
 void missiles_alien_fire(uint16_t type);
 
 
-static missile_t missile_symbols[MISSILE_COUNT] = {
+static missile_t missile_array[MISSILE_COUNT] = {
 		{
 				.symbol = missile0_6x6,			// missile type 1
+				.up = true,
+				.active = false,
 				.size = {.w = MISSILE_WIDTH, .h = MISSILE_HEIGHT}
 		},
 		{
 				.symbol = missile1_6x6,			// missile type 1
+				.up = true,
+				.active = false,
 				.size = {.w = MISSILE_WIDTH, .h = MISSILE_HEIGHT}
 		},
 		{
 				.symbol = missile2_6x6,			// missile type 2
+				.up = true,
+				.active = false,
 				.size = {.w = MISSILE_WIDTH, .h = MISSILE_HEIGHT}
 		},
 		{
 				.symbol = missile3_6x6,
+				.up = true,
+				.active = false,
 				.size = {.w = MISSILE_WIDTH, .h = MISSILE_HEIGHT}
 		},
 };
