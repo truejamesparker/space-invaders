a960a6a72ca9f65bff7d6445ba7fe81aa4f12fbe
jar3dp <brycebfrost@gmail.com>
2015-10-12 13:55:27 -0600

    working on gameScreen lives

--- sw/space_invaders/src/elements/gameScreen.c
+++ sw/space_invaders/src/elements/gameScreen.c
@@ -1,5 +1,9 @@
 #include "gameScreen.h"
 
+// We need these to know what to erase the next time
+static uint8_t currentScoreArray[SCORE_VAL_MAX_LEN] = { 0 };
+static uint8_t currentScoreLength = 0;
+
 void drawFloor();
 void drawScore();
 void drawLives();
@@ -10,28 +14,38 @@ void gameScreen_getScoreAsArray(uint32_t score, uint8_t *scoreArray, uint8_t *sc
 void gameScreen_init() {
 	drawFloor();
 	drawScore();
+	drawLives();
 
 	// draw a '0' next to "SCORE"
 	gameScreen_setScore(0);
+
+	// draw 3 tanks next to "LIVES"
+	gameScreen_setLives(LIVES_MAX);
 }
+
+// ----------------------------------------------------------------------------
 
 void gameScreen_setScore(uint32_t score) {
 	point_t origin = { .x = SCORE_VAL_ORIGIN_X, .y = SCORE_VAL_ORIGIN_Y };
 	symbolsize_t size = { .w = SCORE_VAL_WIDTH, .h = SCORE_VAL_HEIGHT };
 	uint8_t i = 0;
-	uint8_t scoreArray[SCORE_VAL_MAX_LEN] = { 0 };
-	uint8_t scoreLength = 0;
 
 	// Get score info as an array so we can loop through each digit
-	gameScreen_getScoreAsArray(score, scoreArray, &scoreLength);
+	gameScreen_getScoreAsArray(score, currentScoreArray, &currentScoreLength);
 
-	for (i=0; i<scoreLength; i++) {
-		uint32_t digit = scoreArray[i];
+	for (i=0; i<currentScoreLength; i++) {
+		uint32_t digit = currentScoreArray[i];
 		screen_drawSymbol(numbers_5x5[digit], origin, size, SCORE_VAL_SCALE, SCORE_VAL_COLOR);
 
 		// Shift the origin over to the next spot
 		origin.x += (SCORE_VAL_WIDTH*SCORE_VAL_SCALE) + SCORE_VAL_PADDING;
 	}
+}
+
+// ----------------------------------------------------------------------------
+
+void gameScreen_setLives(uint8_t lives) {
+
 }
 
 // ----------------------------------------------------------------------------
@@ -65,8 +79,11 @@ void drawScore() {
 // ----------------------------------------------------------------------------
 
 void drawLives() {
+	point_t origin = { .x = LIVES_TXT_ORIGIN_X, .y = LIVES_TXT_ORIGIN_Y };
+	symbolsize_t size = { .w = LIVES_TXT_WIDTH, .h = LIVES_TXT_HEIGHT };
 
-
+	// Draw the text "SCORE"
+	screen_drawSymbol(word_lives_24x5, origin, size, LIVES_TXT_SCALE, LIVES_TXT_COLOR);
 }
 
 // ----------------------------------------------------------------------------
